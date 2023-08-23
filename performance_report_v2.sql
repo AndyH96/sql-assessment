41950ce36e089be528006796c85d3dcda21c6a88
Andy Ho <andy.hohuu.96@gmail.com>
2023-08-23 16:55:43 +0200

    danill is kind of pretty

---
+++ performance_report_v2.sql
@@ -0,0 +1,30 @@
+with phe_t2_patients as (
+    select 
+        con.contract_name,
+        u.user_id,
+        c.id,
+        c.date_created,
+        date_trunc(c.date_created, MONTH)
+        c.state,
+        c.closure_reason,
+        c.pathway,
+    from `data-warehouse-prod-308513.mart_gb_data_analyst.hcs_gb_case_te` c
+    inner join `data-warehouse-prod-308513.mart_gb_data_analyst.hcs_gb_contract_te` con
+    inner join `data-warehouse-prod-308513.mart_gb_data_analyst.core_user_te` u
+    where c.contract_id in (149, 150, 151, 152, 153, 158, 159, 161, 168, 183, 185, 193, 195)
+    and not lower (concat(u.first_name, u.last_name)) like '%duplicate%'
+    and not lower (concat(u.first_name, u.last_name)) like '%test%'
+    and not lower (u.email) like '@oviva.com' or u.email is null
+),
+ia as (
+    select 
+        ch.case_id,
+        min(ch.date_created)
+    from phe_t2_patients
+    left join `data-warehouse-prod-308513.mart_gb_data_analyst.hcs_gb_case_history_te` ch
+    where ch.state_after = 'TREATMENT_PENDING'
+    group by ch.case_id
+),
+number_of_appts as (
+    select
+
